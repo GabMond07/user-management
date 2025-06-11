24ab0bc1176d1129c365a2503660a890534c63f8
gab <gabcam071118.rg@gmail.com>
2025-06-11 00:45:10 -0600

    Add HttpClient and IonicModule, implement UsuariosService, and enhance page components with improved layouts and functionality

--- gestion-usuarios-angular/src/app/pages/estructuras/estructuras.component.ts
+++ gestion-usuarios-angular/src/app/pages/estructuras/estructuras.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-estructuras',
   templateUrl: './estructuras.component.html',
-  styleUrls: ['./estructuras.component.css']
+  styleUrls: ['./estructuras.component.css'],
+  standalone: false,
+  imports: [IonicModule, FormsModule]
 })
 export class EstructurasComponent {
-
+  mostrar = false;
+  items = ['Elemento 1', 'Elemento 2', 'Elemento 3'];
+  opcion = '';
 }
