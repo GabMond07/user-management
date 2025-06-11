675aef0e1bc13b55f8386cbadef7dc59846db12f
gab <gabcam071118.rg@gmail.com>
2025-06-11 00:15:55 -0600

    Add Bootstrap for styling and configure routing in PagesRoutingModule

--- gestion-usuarios-angular/src/app/pages/pages-routing.module.ts
+++ gestion-usuarios-angular/src/app/pages/pages-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'pages',
+    loadChildren: () => import('./pages.module').then(m => m.PagesModule)
+  },
+  { path: '', redirectTo: 'pages', pathMatch: 'full' },
+  { path: '**', redirectTo: 'pages' }
+];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class PagesRoutingModule { }
+export class AppRoutingModule { }
