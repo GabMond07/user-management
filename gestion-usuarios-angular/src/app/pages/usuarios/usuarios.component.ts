a07728e20327d271b8e445780d19477fa37706d9
gab <gabcam071118.rg@gmail.com>
2025-06-10 23:43:11 -0600

    Add UsuariosService to manage user data retrieval

--- gestion-usuarios-angular/src/app/pages/usuarios/usuarios.component.ts
+++ gestion-usuarios-angular/src/app/pages/usuarios/usuarios.component.ts
@@ -1,14 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuariosService, User } from '../../core/usuarios.service';
+import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-usuarios',
   templateUrl: './usuarios.component.html',
-  styleUrls: ['./usuarios.component.scss'],
+  styleUrls: ['./usuarios.component.css'],
+  standalone: false,
+  imports: [IonicModule]
 })
-export class UsuariosComponent  implements OnInit {
+export class UsuariosComponent implements OnInit {
+  users: User[] = [];
+  loading = true;
 
-  constructor() { }
+  constructor(private usuariosService: UsuariosService) {}
 
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.usuariosService.getUsers().subscribe({
+      next: (data) => {
+        this.users = data;
+        this.loading = false;
+      },
+      error: (err) => {
+        console.error('Error fetching users:', err);
+        this.loading = false;
+      }
+    });
+  }
 }
