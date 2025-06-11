d31e5c7d7d84c8fb40638ff4a75bdfa090050144
gab <gabcam071118.rg@gmail.com>
2025-06-10 23:09:13 -0600

    initial commit

---
+++ gestion-usuarios-angular/src/app/pages/estructuras/estructuras.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-estructuras',
+  templateUrl: './estructuras.component.html',
+  styleUrls: ['./estructuras.component.scss'],
+})
+export class EstructurasComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
